861bb51deb952f29a9b9c089cb47a16f557381ee
Douglas Smith <smithda@gmail.com>
2016-11-11 10:59:04 -0500

    Updating messaging

--- install/cli/yarn.sh
+++ install/cli/yarn.sh
@@ -4,12 +4,12 @@
 # Ubuntu Utilities - yarn
 #===================================================================================
 
-printf "\033[1mInstalling yarn.\033[0m\n"
-
 # Install or Update Yarn
 if [ -e "/usr/local/bin/yarn" ]; then
+    printf "\033[1mInstalling yarn.\033[0m\n"
     npm install -g yarn
 else
+    printf "\033[1mUpdating yarn.\033[0m\n"
     yarn self-update
 fi
     
