f309823c5b42be6d63373f0f3354827ed68ef667
Douglas Smith <smithda@gmail.com>
2016-11-14 16:49:55 -0500

    Updating permissions script

--- install/env/permissions.sh
+++ install/env/permissions.sh
@@ -6,14 +6,16 @@
 
 printf "\033[1mUpdating permissions.\033[0m\n"
 
-# /usr/lib/node_modules
 if [ -e "/usr/lib/node_modules" ]; then
     sudo chown -R $USER /usr/lib/node_modules
 fi
 
-# /usr/lib/node_modules
 if [ -e "/home/administrator/.npm" ]; then
     sudo chown -R $USER:$USER /home/administrator/.npm
+fi
+
+if [ -e "/usr/local/lib" ]; then
+    sudo chown -R $USER /usr/local/lib
 fi
 
 # Wrap it up
