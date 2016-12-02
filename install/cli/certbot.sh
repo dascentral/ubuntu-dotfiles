e0a2cda95ea0082898bfbd9164414269e4823e92
Douglas Smith <smithda@gmail.com>
2016-12-02 10:58:28 -0500

    Adding the certbot installation script

---
+++ install/cli/certbot.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#===================================================================================
+# certbot Install Script - https://certbot.eff.org/
+#===================================================================================
+
+# setup
+name="certbot"
+check="/opt/certbot/certbot-auto"
+
+# Check if installed
+printf "\033[1mInstalling $name...\033[0m"
+if [ -e $check ]; then
+   printf "\033[1m already installed.\n\n\033[0m"
+   exit 0;
+fi
+printf "\n"
+
+# Create Directory
+sudo mkdir /opt/certbot
+sudo chown $USER:$USER /opt/certbot
+
+# Install certbot utility
+wget https://dl.eff.org/certbot-auto -P /opt/certbot
+chmod a+x /opt/certbot/certbot-auto
+
+# Execute Setup
+/opt/certbot/certbot-auto
+
+# Wrap it up
+printf "\n\n"
