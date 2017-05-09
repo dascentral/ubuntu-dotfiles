40579fb7939f446e820c14e9b82e4140568700ef
Douglas Smith <smithda@gmail.com>
2017-05-09 15:39:15 -0400

    Exiting with success if already installed

--- install/cli/nvm.sh
+++ install/cli/nvm.sh
@@ -12,7 +12,7 @@ check="/home/$USER/.nvm"
 printf "\033[1mInstalling $name...\033[0m"
 if [ -e $check ]; then
     printf "\033[1m already installed.\n\n\033[0m"
-    exit 1;
+    exit 0;
 fi
 printf "\n"
 
