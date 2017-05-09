e693dcda6a1671f245730dd685e62ec34a7a40d7
Douglas Smith <smithda@gmail.com>
2017-05-09 15:39:06 -0400

    Whitespacing

--- install/cli/certbot.sh
+++ install/cli/certbot.sh
@@ -11,8 +11,8 @@ check="/opt/certbot/certbot-auto"
 # Check if installed
 printf "\033[1mInstalling $name...\033[0m"
 if [ -e $check ]; then
-   printf "\033[1m already installed.\n\n\033[0m"
-   exit 0;
+    printf "\033[1m already installed.\n\n\033[0m"
+    exit 0;
 fi
 printf "\n"
 
