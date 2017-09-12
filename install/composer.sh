68bb49aec2f780a425f897923c7e0c1f8284e9db
Douglas Smith <smithda@gmail.com>
2017-09-12 13:47:52 -0400

    Adding composer install script

---
+++ install/composer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#===================================================================================
+# Composer - https://getcomposer.org/download/
+#===================================================================================
+
+# setup
+name="Composer"
+check="/usr/local/bin/composer"
+
+# Check if installed
+if [ -e $check ]; then
+    printf "\033[1;37m$name already installed.\n\n\033[0m"
+    exit 0;
+fi
+
+# Install
+printf "\033[1;33mInstalling $name...\033[0m"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+printf "\n\n"
