df8c652e4bc5143a645991cc45f6edd3bcbed6a9
Douglas Smith <smithda@gmail.com>
2017-09-16 17:29:12 -0400

    Adding the updated PGP key for Ubuntu 14.04

--- install/mysql.sh
+++ install/mysql.sh
@@ -23,6 +23,7 @@ printf "\033[1mInstalling $name...\033[0m\n"
 # If running Ubuntu 14.04
 wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
 sudo dpkg -i mysql-apt-config_0.6.0-1_all.deb
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
 sudo apt-get update
 
 # Standard instructions
