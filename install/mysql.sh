f50cab02e6245e765aeedcc58f373c04b85dd3aa
Douglas Smith <smithda@gmail.com>
2017-09-16 09:16:34 -0400

    Adding install steps for Ubuntu 14.04

--- install/mysql.sh
+++ install/mysql.sh
@@ -19,6 +19,13 @@ fi
 
 # Install
 printf "\033[1mInstalling $name...\033[0m\n"
+
+# If running Ubuntu 14.04
+wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
+sudo dpkg -i mysql-apt-config_0.6.0-1_all.deb
+sudo apt-get update
+
+# Standard instructions
 sudo apt-get install -y mysql-server
 sudo mysql_secure_installation
 printf "\n\n"
