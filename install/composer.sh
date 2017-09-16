5c9388af98fc028992d9501dba21626562d6f851
Douglas Smith <smithda@gmail.com>
2017-09-16 09:49:36 -0400

    Updating the Composer install script

--- install/composer.sh
+++ install/composer.sh
@@ -16,9 +16,6 @@ fi
 
 # Install
 printf "\033[1;33mInstalling $name...\033[0m"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 printf "\n\n"
