c54092bf17a09d9b52fb22438bdd43bc96af6c8d
Douglas Smith <smithda@gmail.com>
2017-09-16 09:53:28 -0400

    Updating check for MySQL

--- install/mysql.sh
+++ install/mysql.sh
@@ -3,13 +3,10 @@
 #===================================================================================
 # MySQL
 #===================================================================================
-# Installation instructions via...
-# https://serversforhackers.com/c/lemp-nginx-php-laravel
-#===================================================================================
 
 # Setup
-name="PHP 7.1"
-check="/var/run/php/php7.1-fpm.sock"
+name="MySQL"
+check="/usr/bin/mysql"
 
 # Check if installed
 if [ -e $check ]; then
