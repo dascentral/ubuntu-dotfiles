820d0e377a118fdfeb325474b83d96e4fafdc4f4
Douglas Smith <douglas.smith@akqa.com>
2020-07-05 16:50:00 -0400

    Updating Nginx install

--- install/nginx.sh
+++ install/nginx.sh
@@ -2,21 +2,20 @@
 
 # install Nginx
 if [ ! -e "/etc/nginx" ]; then
-    printf "\033[1;37mInstalling PHP.\n\033[0m"
-    sudo -v
-    sudo apt update
+    printf "\033[1;37mInstalling Nginx.\n\033[0m"
     sudo apt install -y nginx
     sudo ufw allow 'Nginx Full'
 fi
 
 # configure Nginx
 if [ ! -e "/etc/nginx/sites-available/default.original" ]; then
-    printf "\033[1;37mConfiguring Nginx config.\n\033[0m"
-    sudo -v
+    printf "\033[1;37mBacking up default Nginx site.\n\033[0m"
     mkdir -p /var/www/html/server
     mv /var/www/html/index* /var/www/html/server
     cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.original
-    cp ~/utils/resources/nginx/default /etc/nginx/sites-available
-    sudo service nginx reload
     printf "\n\n"
 fi
+
+# copy default site into place
+cp ~/utils/resources/nginx/default /etc/nginx/sites-available
+sudo service nginx reload
