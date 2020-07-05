6ff2cfb539302bc933525876ecfdfcc49fecfe9c
Douglas Smith <douglas.smith@akqa.com>
2020-07-04 21:09:12 -0400

    Installing Nginx via script

--- install/nginx.sh
+++ install/nginx.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+# install Nginx
 if [ ! -e "/etc/nginx" ]; then
-    printf "\n\033[1;33mNginx has not been installed. Aborting installation.\n\n\033[0m"
-    exit 0;
+    printf "\033[1;37mConfiguring Nginx config.\n\033[0m"
+    sudo -v
+    sudo apt update
+    sudo apt install -y nginx
+    sudo ufw allow 'Nginx Full'
 fi
 
+# configure Nginx
 if [ ! -e "/etc/nginx/sites-available/default.original" ]; then
-    printf "\033[1;37mInstalling Nginx config.\033[0m"
+    printf "\033[1;37mConfiguring Nginx config.\n\033[0m"
+    sudo -v
+    mkdir -p /var/www/html/server
+    mv /var/www/html/index* /var/www/html/server
     cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.original
     cp ~/utils/resources/nginx/default /etc/nginx/sites-available
     sudo service nginx reload
