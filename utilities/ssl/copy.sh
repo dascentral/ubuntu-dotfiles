5db103b279c8362551f0ef7fdb585e18875c5dec
Douglas Smith <douglas.smith@akqa.com>
2020-08-14 14:00:28 -0400

    WIP

--- utilities/ssl/copy.sh
+++ utilities/ssl/copy.sh
@@ -20,7 +20,7 @@ printf "\n"
 # Collect information
 printf "\033[1mWhat domain are we transferring?\033[0m\n"
 read -p 'Domain: ' domain
-if [ ! -e "/etc/letsencrypt/archive/${domain}" ]; then
+if sudo [ ! -e "/etc/letsencrypt/archive/${domain}" ]; then
     printf "\n\033[1;33mUnable to find certificates for ${domain}.\n\n\033[0m"
     exit 1;
 fi
