d67887808544b635fcd226709a1ea88046d8a19b
Douglas Smith <douglas.smith@akqa.com>
2022-04-04 18:02:31 -0400

    Removing refactored files

--- install/certbot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -e "/usr/bin/certbot" ]; then
-    printf "\033[1;37mInstalling Certbot.\n\033[0m"
-    sudo apt install software-properties-common
-    sudo add-apt-repository universe
-    sudo apt update
-    sudo apt install -y certbot python3-certbot-nginx
-fi
--- install/redis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ ! -e "/etc/redis" ]; then
-    printf "\033[1;37mInstalling Redis.\n\033[0m"
-    sudo apt install -y redis-server
-fi
