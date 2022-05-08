7ac0e122248296a76c27c8868fd804882a156518
Douglas Smith <smithda@gmail.com>
2022-05-08 11:14:04 -0500

    WIP

--- install/certbot.sh
+++ install/certbot.sh
@@ -10,3 +10,16 @@ if [ ! -e "/usr/bin/certbot" ]; then
     sudo apt install -y certbot python3-certbot-nginx
     printf "\n"
 fi
+
+#https://certbot.eff.org/instructions?ws=nginx&os=ubuntubionic
+#sudo apt install snapd
+#sudo snap install core; sudo snap refresh core
+#sudo apt-get remove certbot
+
+#uninstalling certbot
+#remove from crontab
+#sudo rm -rf /opt/certbot/certbot-auto
+#sudo rm -rf /opt/eff.org
+
+#preparing new certbot
+#sudo ln -s /snap/bin/certbot /usr/bin/certbot
