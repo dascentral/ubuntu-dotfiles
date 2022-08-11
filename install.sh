9c6dcbf567437cebeb52d2760fc00109d22bc381
Douglas Smith <smithda@gmail.com>
2022-08-11 14:05:23 -0400

    Making some software installations optional

--- install.sh
+++ install.sh
@@ -3,9 +3,31 @@
 source ${HOME}/.dotfiles/shell/.functions
 
 ./install/utilities.sh
-./install/nginx.sh
-./install/php.sh
-./install/composer.sh
-./install/redis.sh
 ./install/certbot.sh
 ./install/oh-my-zsh.sh
+
+# END global software install
+###########################################################################################
+# BEGIN optional software install
+
+echo -n "Install Nginx? (y/N) "
+read -r -n1 install
+printf "\n"
+if [ "$install" = "y" ]; then
+    ./install/nginx.sh
+fi
+
+echo -n "Install PHP & Composer? (y/N) "
+read -r -n1 install
+printf "\n"
+if [ "$install" = "y" ]; then
+    ./install/php.sh
+    ./install/composer.sh
+fi
+
+echo -n "Install Redis? (y/N) "
+read -r -n1 install
+printf "\n"
+if [ "$install" = "y" ]; then
+    ./install/redis.sh
+fi
