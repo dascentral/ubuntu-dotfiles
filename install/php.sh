578c878a2ecd8d8f027ef48a30b25f73d1880d26
Douglas Smith <smithda@gmail.com>
2022-12-23 07:23:38 -0500

    Updating to PHP 8.2

--- install/php.sh
+++ install/php.sh
@@ -2,37 +2,45 @@
 
 source ${HOME}/.dotfiles/shell/.functions
 
-installPHP() {
+installPHP82() {
     sudo apt install -y php php-fpm php-cli php-mysql
+    sudo apt install -y php8.2-curl php8.2-gd php8.2-intl php8.2-ldap php8.2-mbstring php8.2-xml php8.2-zip
+}
+
+installPHP81() {
+    sudo apt install -y php8.1 php8.1-fpm php8.1-cli php8.1-mysql
     sudo apt install -y php8.1-curl php8.1-gd php8.1-intl php8.1-ldap php8.1-mbstring php8.1-xml php8.1-zip
 }
 
+installPHP80() {
+    sudo apt install -y php8.0 php8.0-fpm php8.0-cli php8.0-mysql
+    sudo apt install -y php8.0-curl php8.0-gd php8.0-intl php8.0-ldap php8.0-mbstring php8.0-xml php8.0-zip
+}
+
 if [ ! -e "/etc/php" ]; then
-    info "Installing PHP 8.1."
-    installPHP
+    info "Installing PHP 8.2."
+    installPHP82
     printf "\n"
 else
-    info "PHP 8.1 is already installed."
+    info "PHP 8.2 is already installed."
     echo -n "Reinstall? (y/N) "
     read -r -n1 reinstall
     printf "\n"
     if [ "$reinstall" = "y" ]; then
         printf "\n"
-        installPHP
+        installPHP82
         printf "\n"
     fi
 fi
 
-if [ ! -e "/etc/php/8.0" ]; then
-    info "Installing PHP 8.0."
-    sudo apt install -y php8.0 php8.0-fpm php8.0-mysql
-    sudo apt install -y php8.0-curl php8.0-intl php8.0-ldap php8.0-mbstring php8.0-xml php8.0-zip
+if [ ! -e "/etc/php/8.1" ]; then
+    info "Installing PHP 8.1."
+    installPHP81
     printf "\n"
 fi
 
-if [ ! -e "/etc/php/7.4" ]; then
-    info "Installing PHP 7.4."
-    sudo apt install -y php7.4 php7.4-fpm php7.4-mysql
-    sudo apt install -y php7.4-curl php7.4-intl php7.4-ldap php7.4-mbstring php7.4-xml php7.4-zip
+if [ ! -e "/etc/php/8.0" ]; then
+    info "Installing PHP 8.0."
+    installPHP80
     printf "\n"
 fi
