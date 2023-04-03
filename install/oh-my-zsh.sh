23f9c3c925f0c7490fa3f7b2085d5d567b0c9041
Douglas Smith <smithda@gmail.com>
2023-04-02 22:55:59 -0400

    WIP

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -4,6 +4,7 @@ source ${HOME}/.dotfiles/shell/.functions
 
 if [ ! -e "${HOME}/.oh-my-zsh" ]; then
     info "Installing Oh My Zsh"
+    sudo apt install -y zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     echo ""
 fi
