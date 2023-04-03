428e51be8aab98b7d7a04cfdf3042c281c1754b0
Douglas Smith <smithda@gmail.com>
2023-04-03 15:15:14 -0400

    WIP

--- install/nginx.sh
+++ install/nginx.sh
@@ -2,31 +2,50 @@
 
 source ${HOME}/.dotfiles/shell/.functions
 
+# execute only if Nginx is not already installed
 if [ ! -e "/etc/nginx" ]; then
-    info "Installing Nginx."
-    sudo apt install -y nginx
-    sudo ufw allow 'Nginx Full'
+    echo -n "Install Nginx? (y/N) "
+    read -r -n1 install
+    echo ""
+    if [ "$install" = "y" ]; then
+        info "Installing Nginx."
+        sudo apt install -y nginx
+        sudo ufw allow 'Nginx Full'
+    fi
 fi
 
 # END installation
 ###########################################################################################
 # BEGIN configuration
 
-mkdir -p /var/www/html/server
-mkdir -p /var/www/html/dev
-mkdir -p /var/www/html/stage
-mkdir -p /var/www/html/uat
-mkdir -p /var/www/html/prod
+# only configure if Nginx is installed
+if [ -e "/etc/nginx" ]; then
+    echo "Configure Nginx";
 
-# backup initial default configuration
-if [ ! -e "/etc/nginx/sites-available/default.original" ]; then
-    printf "\033[1;37mBacking up default Nginx site.\n\033[0m"
-    cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.original
-    mkdir -p /var/www/html/server
-    mv /var/www/html/index* /var/www/html/server
-    printf "\n\n"
+    # update ownership of these locations
+    # sudo chown -R $USER:$USER /etc/nginx/sites-available
+    # sudo chown -R $USER:$USER /etc/nginx/sites-enabled
+
+    # copy default Nginx site into its own folder
+    # if [ ! -e "/var/www/html/server" ]; then
+    #     printf "\033[1;37mMoving default Nginx site into '/var/www/html/server'.\n\033[0m"
+
+    #     # copy default site to new location
+    #     mkdir -p /var/www/html/server
+    #     cp /var/www/html/index* /var/www/html/server
+
+    #     # back up default Nginx config
+    #     cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.original
+
+    #     # copy new config into place
+    #     cp ${DOTFILES}/config/nginx/default /etc/nginx/sites-available
+
+    #     # reload Nginx
+    #     sudo service nginx reload
+
+    #     # remove original default site files
+    #     rm /var/www/html/index*
+
+    #     echo ""
+    # fi
 fi
-
-# copy default site into place
-cp ${DOTFILES}/config/nginx/default /etc/nginx/sites-available
-sudo service nginx reload
