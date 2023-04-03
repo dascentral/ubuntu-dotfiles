266ae675dee9ad21aac406ec5b086b5e5dc9af72
Douglas Smith <smithda@gmail.com>
2023-04-03 12:01:19 -0400

    WIP

--- install/utilities.sh
+++ install/utilities.sh
@@ -5,59 +5,59 @@ source ${HOME}/.dotfiles/shell/.functions
 if [ ! -e "/usr/bin/curl" ]; then
     info "Installing curl."
     sudo apt install -y curl
-    print "\n"
+    echo ""
 fi
 
 if [ ! -e "/usr/bin/git" ]; then
     info "Installing Git."
     sudo apt install -y git
-    print "\n"
+    echo ""
 fi
 
 if [ ! -e "/usr/bin/htop" ]; then
     info "Installing htop."
     sudo apt install -y htop
-    print "\n"
+    echo ""
 fi
 
 if [ ! -e "/usr/bin/ncdu" ]; then
     info "Installing ncdu."
     sudo apt install -y ncdu
-    print "\n"
+    echo ""
 fi
 
 if [ ! -e "/usr/bin/ag" ]; then
     info "Installing ag."
     sudo apt install -y silversearcher-ag
-    print "\n"
+    echo ""
 fi
 
 if [ ! -e "/usr/bin/tmux" ]; then
     info "Installing tmux."
     sudo apt install -y tmux
-    print "\n"
+    echo ""
 fi
 
 if [ ! -e "/usr/bin/unzip" ]; then
     info "Installing unzip."
     sudo apt install -y unzip
-    print "\n"
+    echo ""
 fi
 
 if [ ! -e "/usr/bin/vim" ]; then
     info "Installing vim."
     sudo apt install -y vim
-    print "\n"
+    echo ""
 fi
 
 if [ ! -e "/usr/bin/wget" ]; then
     info "Installing wget."
     sudo apt install -y wget
-    print "\n"
+    echo ""
 fi
 
 if [ ! -e "/usr/bin/zip" ]; then
     info "Installing zip."
     sudo apt install -y zip
-    print "\n"
+    echo ""
 fi
