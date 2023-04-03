29440809f2d8baf72ef83b01cb0a17c730980590
Douglas Smith <smithda@gmail.com>
2023-04-03 15:40:24 -0400

    WIP

--- install/php.sh
+++ install/php.sh
@@ -5,6 +5,6 @@ source ${HOME}/.dotfiles/shell/.functions
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 
-./install/php82.sh
-./install/php81.sh
 ./install/php80.sh
+./install/php81.sh
+./install/php82.sh
